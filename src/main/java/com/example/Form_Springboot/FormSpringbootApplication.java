3d24a649b57cc2cc7cad5d41fc4c9bb9a4580597
ParimalTank <parimaltank2001@gmail.com>
2021-09-18 17:38:53 +0530

    ADD

---
+++ src/main/java/com/example/Form_Springboot/FormSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.Form_Springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Form_Springboot/FormSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Form_Springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
